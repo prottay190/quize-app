cc9fb9086ca4cf14da2880bff6cc4af2e139254f
unknown <aarnob2020@gmail.com>
2021-12-16 13:27:19 -0800

    done

--- screen/Quize.js
+++ screen/Quize.js
@@ -4,36 +4,37 @@ import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 const Quize = ({navigation}) =>{
     const [qustion, setQuistion] = useState([]);
-    const [quize, setQuize] = useState(0);
-    const getQuize = async() =>{
-        const url = 'https://opentdb.com/api.php?amount=10&type=multiple';
-        const res = await fetch(url);
-        const data = await res.json();
-        setQuistion(data.results)
-    }
+   
     useEffect(()=>{
-        getQuize()
+        fetch('https://opentdb.com/api.php?amount=10&type=multiple')
+        .then(res => res.json())
+        .then(data => setQuistion(data.results))
     },[])
+    
  return(
      <View style={styles.container}>
-  { qustion && <View style={styles.parent}>    
+  { qustion. map( ques =>
+      <View style={styles.parent}>    
       <View>
-       <Text style={styles.ques}>Q. Select currect option </Text>
+       <Text style={styles.ques}>Q. {ques.question} </Text>
        </View>
        <View style={styles.options}>
            <TouchableOpacity style={styles.opt}>
-               <Text style={styles.q}>option 1</Text>
+               <Text style={styles.q}>{ques.correct_answer}</Text>
             </TouchableOpacity>
            <TouchableOpacity style={styles.opt}>
-               <Text style={styles.q}>option 2</Text>
+               <Text style={styles.q}>{ques.incorrect_answers[0]}</Text>
             </TouchableOpacity>
            <TouchableOpacity style={styles.opt}>
-               <Text style={styles.q}>option 3</Text>
+               <Text style={styles.q}>{ques.incorrect_answers[1]}</Text>
             </TouchableOpacity>
            <TouchableOpacity style={styles.opt}>
-               <Text style={styles.q}>option 4</Text>
+               <Text style={styles.q}>{ques.incorrect_answers[2]}</Text>
             </TouchableOpacity>
        </View>
+       </View>
+  ) 
+       }
        <View style={styles.btn}>
            <TouchableOpacity onPress={() =>navigation.navigate("Home")} style={styles.button}>
                <Text style={styles.buttonText}>SKIP</Text>
@@ -42,8 +43,6 @@ const Quize = ({navigation}) =>{
                 <Text style={styles.buttonText}>Next</Text>
            </TouchableOpacity>
        </View>
-       </View>
-       }
      </View>
      
  )
